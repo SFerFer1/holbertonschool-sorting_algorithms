985708eead3ef3ba07901fd6d9a44a59d636f1ec
SFerFer1 <10048@holbertonstudents.com>
2024-12-12 13:31:19 +0000

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,11 +23,11 @@ void bubble_sort(int *array, size_t size)
 	{
 		if (array[i] > array[i + 1])
 		{
+		print_array(array, size);
 		temp  = array[i];
 		array[i] = array[i + 1];
 		array[i + 1] = temp;
 		comprovar = false;
-		print_array(array, size);
 		}
 	}
 	}
