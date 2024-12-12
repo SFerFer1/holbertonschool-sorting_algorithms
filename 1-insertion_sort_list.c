9433bb89262a089008826fbfdbbad55bc3b9b94d
SFerFer1 <10048@holbertonstudents.com>
2024-12-12 17:35:34 +0000

    aa

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+/**
+ * insertion_sort_list -using the Bubble sort algorithm.
+ *
+ * @list: the list you want to sort.
+ *
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+	current = (*list)->next;
+
+	while (current->prev != NULL && current->n < current->prev->n)
+        {
+		temp = current->prev;
+		temp->next = current->next;
+            if (current->next != NULL)
+                current->next->prev = temp;
+
+	    current->prev = temp->prev;
+            if (temp->prev != NULL)
+                temp->prev->next = current;
+
+}
