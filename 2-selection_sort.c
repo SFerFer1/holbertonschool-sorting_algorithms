aba39e0d73a0e52567d674cfaac181ae3558174b
SFerFer1 <10048@holbertonstudents.com>
2024-12-14 21:35:22 +0000

    Hot fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,8 +9,29 @@
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i, j, min;
+	int  temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min_idx != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
 
 
 
 
 }
+}
+
