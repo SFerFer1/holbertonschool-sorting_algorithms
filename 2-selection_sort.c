ee9c57140bf38dc8917ca5a899bd788de2b9e4aa
SFerFer1 <10048@holbertonstudents.com>
2024-12-14 14:45:46 +0000

    Hot fix

---
+++ 2-selection_sort.c
@@ -0,0 +1,16 @@
+#include "sort.h"
+/**
+ * selection_sort -using the Bubble sort algorithm.
+ *
+ * @array: the list you want to sort.
+ *
+ * @size: the list you want to sort.
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+
+
+
+
+}
